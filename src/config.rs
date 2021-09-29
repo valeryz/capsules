35800e1433af69ac86e0a5b2ce9d829757771f72
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-09-29 08:42:45 +0200

    Fixing some bugs in the config

--- src/config.rs
+++ src/config.rs
@@ -2,11 +2,13 @@ use anyhow::{bail, Result};
 use clap::{App, Arg};
 use itertools;
 use serde::Deserialize;
-use std::collections::HashMap;
+use sha2::digest::generic_array::typenum::private::IsEqualPrivate;
+use std::collections::BTreeMap;
+use std::path::Path;
 use std::{env, ffi::OsString};
 use toml;
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub enum Milestone {
     Placebo,
     BluePill,
@@ -14,13 +16,13 @@ pub enum Milestone {
     RedPill,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub enum Backend {
     Stdio,
     Honeycomb,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug)]
 pub struct Config {
     pub milestone: Milestone,
     pub backend: Backend,
@@ -28,9 +30,31 @@ pub struct Config {
     pub input_files: Vec<OsString>,
     pub tool_tags: Vec<OsString>,
     pub output_files: Vec<OsString>,
-    pub capture_stdout: bool,
-    pub capture_stderr: bool,
+    pub capture_stdout: Option<bool>,
+    pub capture_stderr: Option<bool>,
     pub command_to_run: Vec<OsString>,
+}
+
+impl std::ops::Deref for Config {
+    type Target = Option<OsString>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.capsule_id
+    }
+}
+
+impl From<StringConfig> for Config {
+    fn from(config: StringConfig) -> Self {
+        Config {
+            capsule_id: config.capsule_id.map(OsString::from),
+            input_files: config.input_files.iter().map(OsString::from).collect(),
+            tool_tags: config.tool_tags.iter().map(OsString::from).collect(),
+            output_files: config.output_files.iter().map(OsString::from).collect(),
+            capture_stdout: config.capture_stdout,
+            capture_stderr: config.capture_stderr,
+            ..Config::default()
+        }
+    }
 }
 
 impl Default for Config {
@@ -42,44 +66,87 @@ impl Default for Config {
             input_files: vec![],
             tool_tags: vec![],
             output_files: vec![],
-            capture_stdout: false,
-            capture_stderr: false,
+            capture_stdout: Some(false),
+            capture_stderr: Some(false),
             command_to_run: vec![],
         }
     }
 }
 
+// We use this struct for Toml deserialization, because Serde deserializes
+// OsString in a very weird way, therefore we deserialize into Strings,
+// later converting them into OsString.
+#[derive(Deserialize, Debug)]
+struct StringConfig {
+    #[serde(default)]
+    pub capsule_id: Option<String>,
+
+    #[serde(default)]
+    pub input_files: Vec<String>,
+
+    #[serde(default)]
+    pub tool_tags: Vec<String>,
+
+    #[serde(default)]
+    pub output_files: Vec<String>,
+
+    #[serde(default)]
+    pub capture_stdout: Option<bool>,
+
+    #[serde(default)]
+    pub capture_stderr: Option<bool>,
+}
+
 impl Config {
+    // Merge one config (e.g. Capsule.toml) into another (~/.capsule.toml)
+    // It destroys the argument.
+    pub fn merge(&mut self, config: &mut Self) {
+        if self.capsule_id.is_none() {
+            self.capsule_id = config.capsule_id.take();
+        }
+        self.input_files.append(&mut config.input_files);
+        self.output_files.append(&mut config.output_files);
+        self.tool_tags.append(&mut config.tool_tags);
+    }
+
     pub fn new<I, T>(
         cmdline_args: I,
-        default_toml: Option<&std::path::Path>,
-        current_toml: Option<&std::path::Path>,
+        default_toml: Option<&Path>,
+        current_toml: Option<&Path>,
     ) -> Result<Self>
     where
         I: IntoIterator<Item = T>,
         T: Into<OsString> + Clone,
     {
+        // Read the defaults TOML (usually from ~/.capsule.toml).
         let mut config = Self::default();
         if let Some(default_toml) = default_toml {
             if let Ok(contents) = std::fs::read_to_string(default_toml) {
-                if let Ok(home_config) = toml::from_str(&contents) {
-                    config = home_config;
+                if let Ok(home_config) = toml::from_str::<StringConfig>(&contents) {
+                    config = Config::from(home_config);
                 }
             }
         }
 
-        let mut dir_config = HashMap::<String, Config>::new();
+        // Read the main TOML (usually from Capsule.toml in the current directory).
+        let mut dir_config: BTreeMap<String, Config> = BTreeMap::new();
         if let Some(current_toml) = current_toml {
             if let Ok(contents) = std::fs::read_to_string(current_toml) {
-                if let Ok(config) = toml::from_str(&contents) {
-                    dir_config = config;
-                } else {
-                    bail!("Could not parse Capsules.toml");
+                match toml::from_str::<BTreeMap<String, StringConfig>>(&contents) {
+                    Ok(config) => {
+                        dir_config = config
+                            .into_iter()
+                            .map(|(key, value)| (key, Config::from(value)))
+                            .collect();
+                    }
+                    Err(e) => {
+                        bail!("Could not parse Capsules.toml: {}", e)
+                    }
                 }
             }
         }
 
-        // Command line.
+        // Read the command line from both os::args and the environment.
         let arg_matches = App::new("Capsules")
             .version("1.0")
             .arg(
@@ -157,6 +224,16 @@ impl Config {
             }
         }
 
+        // Take capsule_is as String (we get lots of OsString here!)
+        let capsule_id: &OsString = config.capsule_id.as_ref().unwrap();
+        let capsule_id: OsString = capsule_id.clone();
+        let capsule_id: String = capsule_id.into_string().unwrap();
+
+        // dir_config can have many sections, relating to manu capsules. We just need one.
+        if let Some(mut single_config) = dir_config.remove(&capsule_id) {
+            config.merge(&mut single_config);
+        }
+
         for matches in match_sources {
             if let Some(inputs) = matches.values_of_os("input") {
                 config.input_files.extend(inputs.map(|x| x.to_owned()));
@@ -168,10 +245,10 @@ impl Config {
                 config.output_files.extend(outputs.map(|x| x.to_owned()));
             }
             if matches.is_present("stdout") {
-                config.capture_stdout = true;
+                config.capture_stdout = Some(true);
             }
             if matches.is_present("stderr") {
-                config.capture_stderr = true;
+                config.capture_stderr = Some(true);
             }
             if let Some(command) = matches.values_of_os("command_to_run") {
                 config.command_to_run = command.map(|x| x.to_os_string()).collect();
@@ -189,13 +266,16 @@ impl Config {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
     use serial_test::serial;
+    use std::io::Write;
     use std::iter;
+    use tempfile::NamedTempFile;
 
     const EMPTY_ARGS: iter::Empty<OsString> = std::iter::empty::<OsString>();
 
     #[test]
-    #[serial]
+    #[serial] // Must serialize these tests so that env vars don't affect other tests.
     fn test_command_line_1() {
         env::set_var("CAPSULE_ARGS", "-c my_capsule -- /bin/echo");
         let config = Config::new(EMPTY_ARGS, None, None).unwrap();
@@ -224,7 +304,26 @@ mod tests {
     }
 
     #[test]
-    fn test_toml() {}
+    fn test_toml() {
+        let mut config_file = NamedTempFile::new().unwrap();
+        let config_contents: &'static str = indoc! {r#"
+           [my_capsule]
+           output_files=["compiled_binary"]
+           input_files=["/etc/passwd", "/nonexistent"]
+        "#};
+        println!("Config file:\n{}", config_contents);
+        config_file.write(config_contents.as_bytes()).unwrap();
+        config_file.flush().unwrap();
+
+        let config = Config::new(
+            vec!["placebo", "-c", "my_capsule", "--", "/bin/echo"],
+            None,
+            Some(config_file.path()),
+        )
+        .unwrap();
+        assert_eq!(config.input_files, vec!["/etc/passwd", "/nonexistent"]);
+        assert_eq!(config.output_files, vec!["compiled_binary"]);
+    }
 
     #[test]
     fn test_toml_defaults() {}
