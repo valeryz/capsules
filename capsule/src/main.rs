e64b5a6b94371466138e8e4db28176708031085c
Marko Kosmerl <marko.kosmerl@dfinity.org>
2021-10-13 12:59:12 +0200

    Initial changes

--- capsule/src/main.rs
+++ capsule/src/main.rs
@@ -1,5 +1,3 @@
-use std::env;
-use std::path::Path;
 use anyhow::Result;
 use capsule::caching::backend::CachingBackend;
 use capsule::caching::honeycomb;
@@ -7,14 +5,22 @@ use capsule::caching::stdio;
 use capsule::capsule::Capsule;
 use capsule::config::{Backend, Config};
 use capsule::wrapper;
+use std::env;
+use std::path::Path;
 
 fn capsule_main() -> Result<()> {
-    let default_toml =  std::env::var("HOME").ok().and_then(|home| Some(home + "/.capsules.toml"));
-    let config = Config::new(env::args(),
-                             default_toml.as_ref().map(Path::new),
-                             Some(Path::new("Capsule.toml").as_ref()))?;
-    let backend : Box<dyn CachingBackend> = match config.backend {
-        Backend::Stdio => Box::new(stdio::StdioBackend {verbose_output: config.verbose}),
+    let default_toml = std::env::var("HOME")
+        .ok()
+        .and_then(|home| Some(home + "/.capsules.toml"));
+    let config = Config::new(
+        env::args(),
+        default_toml.as_ref().map(Path::new),
+        Some(Path::new("Capsule.toml").as_ref()),
+    )?;
+    let backend: Box<dyn CachingBackend> = match config.backend {
+        Backend::Stdio => Box::new(stdio::StdioBackend {
+            verbose_output: config.verbose,
+        }),
         Backend::Honeycomb => Box::new(honeycomb::HoneycombBackend {}),
     };
     let capsule = Capsule::new(&config, backend);
--- capsule/src/wrapper.rs
+++ capsule/src/wrapper.rs
@@ -1,11 +1,11 @@
+use anyhow::{anyhow, Result};
+use itertools;
+use nix::unistd::execvp;
 use std::env;
 use std::ffi::CString;
-use itertools;
-use anyhow::{anyhow, Result};
-use nix::unistd::execvp;
+use std::process::Command;
 
-static USAGE: &'static str =
-    "Usage: capsule <capsule arguments ...> -- command [<arguments>]";
+static USAGE: &'static str = "Usage: capsule <capsule arguments ...> -- command [<arguments>]";
 
 pub fn exec_program<I>(program_name: String, args: I) -> Result<()>
 where
@@ -14,17 +14,19 @@ where
     let program_cstring = CString::new(program_name)?;
     let args: Vec<String> = args.collect();
     println!("Executing {:?}", args);
-    let arg_cstrings = args.into_iter().map(CString::new).collect::<Result<Vec<_>, _>>()?;
+    let arg_cstrings = args
+        .into_iter()
+        .map(CString::new)
+        .collect::<Result<Vec<_>, _>>()?;
 
     match execvp(&program_cstring, &arg_cstrings) {
         Ok(_) => unreachable!(),
-        Err(error) => Err(error.into())
+        Err(error) => Err(error.into()),
     }
 }
 
-
 /// Execute a given command transparently passing the original arguments.
-pub fn execute() -> Result<()> {
+pub fn execute_legacy() -> Result<()> {
     let mut args = env::args();
     let argv0 = &mut args.next();
     if argv0.is_none() {
@@ -38,8 +40,37 @@ pub fn execute() -> Result<()> {
     }
 
     if let Some(program_name) = args.next() {
-        exec_program(program_name.clone(), itertools::chain!([program_name], args))
+        exec_program(
+            program_name.clone(),
+            itertools::chain!([program_name], args),
+        )
     } else {
         Err(anyhow!(USAGE.to_string()))
     }
+}
+
+pub fn execute() -> Result<()> {
+    let mut args = env::args();
+    let argv0 = &mut args.next();
+    if argv0.is_none() {
+        return Err(anyhow!(USAGE));
+    }
+    // Consume the rest of the arguments until we have the -- part
+    for arg in args.by_ref() {
+        if arg == "--" {
+            break;
+        }
+    }
+
+    let args: Vec<String> = args.collect();
+    println!("Executing {:?}", args);
+    let s: String = args.join(" ");
+    let mut p = Command::new("/bin/bash")
+        .arg("-c")
+        .arg(s)
+        .spawn()
+        .expect("failed to execute process");
+    let ret = p.wait()?;
+    println!("Return: {}", ret);
+    Ok(())
 }
