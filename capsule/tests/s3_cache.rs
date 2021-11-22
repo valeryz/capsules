e29dc9aa87df6f432e3d29cf59c09d89e738bf8a
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-11-22 07:49:21 +0000

    Reduce test flakiness

--- capsule/tests/s3_cache.rs
+++ capsule/tests/s3_cache.rs
@@ -5,7 +5,10 @@ fn test_s3_cache_miss() {
     let setup_data = common::setup(); // RAII - clean up on destruction.
     let path = setup_data.path("output.txt");
     let command = format!("echo 'wtf' > {}", path.to_str().unwrap());
-    common::capsule(&["-c", "wtf", "-b", "s3", "--", "/bin/bash", "-c", &command]);
+    common::capsule(
+        setup_data.port,
+        &["-c", "wtf", "-b", "s3", "--", "/bin/bash", "-c", &command],
+    );
     println!("Checking file {:?}", path);
     assert!(path.exists());
 }
@@ -19,18 +22,21 @@ fn test_s3_cache_hit() {
     let side_effect = setup_data.path("side_effect.txt");
     let command = format!("echo 'hello!' > {}", side_effect.to_str().unwrap());
     // Run it first time.
-    common::capsule(&[
-        "-c",
-        "wtf",
-        "-b",
-        "s3",
-        "-i",
-        input.to_str().unwrap(),
-        "--",
-        "/bin/bash",
-        "-c",
-        &command,
-    ]);
+    common::capsule(
+        setup_data.port,
+        &[
+            "-c",
+            "wtf",
+            "-b",
+            "s3",
+            "-i",
+            input.to_str().unwrap(),
+            "--",
+            "/bin/bash",
+            "-c",
+            &command,
+        ],
+    );
     // Creating
     println!("Checking file {:?}", side_effect);
     assert!(side_effect.exists());
@@ -39,18 +45,21 @@ fn test_s3_cache_hit() {
     // Run it second time.
     let side_effect = setup_data.path("side_effect_2.txt");
     let command = format!("echo 'wtf' > {}", side_effect.to_str().unwrap());
-    common::capsule(&[
-        "-c",
-        "wtf",
-        "-b",
-        "s3",
-        "-i",
-        input.to_str().unwrap(),
-        "--",
-        "/bin/bash",
-        "-c",
-        &command,
-    ]);
+    common::capsule(
+        setup_data.port,
+        &[
+            "-c",
+            "wtf",
+            "-b",
+            "s3",
+            "-i",
+            input.to_str().unwrap(),
+            "--",
+            "/bin/bash",
+            "-c",
+            &command,
+        ],
+    );
     println!("Checking file {:?}", side_effect);
     // Verify that the second time the side effect is absent.
     // TODO: this is not yet true in placebo!
@@ -60,26 +69,27 @@ fn test_s3_cache_hit() {
 #[test]
 #[ignore]
 fn test_cache_expiration() {
-    let _setup_data = common::setup(); // RAII - clean up on destruction.
-
     let setup_data = common::setup(); // RAII - clean up on destruction.
     let input = setup_data.path("input.txt");
 
     let side_effect = setup_data.path("side_effect.txt");
     let command = format!("echo 'hello!' > {}", side_effect.to_str().unwrap());
     // Run it first time.
-    common::capsule(&[
-        "-c",
-        "wtf",
-        "-t",
-        "foo",
-        "-i",
-        input.to_str().unwrap(),
-        "--",
-        "/bin/bash",
-        "-c",
-        &command,
-    ]);
+    common::capsule(
+        setup_data.port,
+        &[
+            "-c",
+            "wtf",
+            "-t",
+            "foo",
+            "-i",
+            input.to_str().unwrap(),
+            "--",
+            "/bin/bash",
+            "-c",
+            &command,
+        ],
+    );
     // Creating
     println!("Checking file {:?}", side_effect);
     assert!(side_effect.exists());
@@ -90,18 +100,21 @@ fn test_cache_expiration() {
     // Run it second time.
     let side_effect = setup_data.path("side_effect_2.txt");
     let command = format!("echo 'wtf' > {}", side_effect.to_str().unwrap());
-    common::capsule(&[
-        "-c",
-        "wtf",
-        "-t",
-        "foo",
-        "-i",
-        input.to_str().unwrap(),
-        "--",
-        "/bin/bash",
-        "-c",
-        &command,
-    ]);
+    common::capsule(
+        setup_data.port,
+        &[
+            "-c",
+            "wtf",
+            "-t",
+            "foo",
+            "-i",
+            input.to_str().unwrap(),
+            "--",
+            "/bin/bash",
+            "-c",
+            &command,
+        ],
+    );
     println!("Checking file {:?}", side_effect);
     // Verify that the second time the side effect is absent.
     assert!(side_effect.exists());
