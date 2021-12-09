c38307c88d39ab670fe9c63beae578236fe1a2be
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-12-09 10:32:40 +0100

    Make sure fallback execution on error happens even if the error is in the beginning of config parsing

--- capsule/src/main.rs
+++ capsule/src/main.rs
@@ -13,36 +13,41 @@ use std::path::Path;
 use std::process;
 use std::sync::atomic::{AtomicBool, Ordering};
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let default_toml = std::env::var("HOME").ok().map(|home| home + "/.capsules.toml");
-    let config = Config::new(
-        env::args(),
-        default_toml.as_ref().map(Path::new),
-        Some(Path::new("Capsule.toml")),
-    )?;
-    // First, instantiate our caching backend (S3, Dummy, or possibly other in the future).
-    let backend: Box<dyn CachingBackend> = match config.backend {
-        Backend::Dummy => Box::new(dummy::DummyBackend {
-            verbose_output: config.verbose,
-            capsule_id: config.capsule_id.as_ref().cloned().unwrap(),
-        }),
-        Backend::S3 => Box::new(s3::S3Backend::from_config(&config)?),
-    };
-    // Instantiate our logger (for observability)
-    let logger: Box<dyn Logger> = if config.honeycomb_dataset.is_some() {
-        Box::new(honeycomb::Honeycomb::from_config(&config)?)
-    } else {
-        Box::new(DummyLogger)
-    };
-
-    let capsule = Capsule::new(&config, backend.as_ref(), logger.as_ref());
-
     // Running of the capsule may fail. It may fail either before the wrapped program
     // was run, or after. This flag says whether the program was actually run.
     let mut program_run = AtomicBool::new(false);
-    let result = capsule.run_capsule(&mut program_run).await;
+    let program_run_ref = &mut program_run;
+    // Place all the initialization logic is a separate block, so that the ? bailouts
+    // return the result right there.
+    let result = async move {
+        let default_toml = std::env::var("HOME").ok().map(|home| home + "/.capsules.toml");
+        let config = Config::new(
+            env::args(),
+            default_toml.as_ref().map(Path::new),
+            Some(Path::new("Capsule.toml")),
+        )?;
+        // First, instantiate our caching backend (S3, Dummy, or possibly other in the future).
+        let backend: Box<dyn CachingBackend> = match config.backend {
+            Backend::Dummy => Box::new(dummy::DummyBackend {
+                verbose_output: config.verbose,
+                capsule_id: config.capsule_id.as_ref().cloned().unwrap(),
+            }),
+            Backend::S3 => Box::new(s3::S3Backend::from_config(&config)?),
+        };
+        // Instantiate our logger (for observability)
+        let logger: Box<dyn Logger> = if config.honeycomb_dataset.is_some() {
+            Box::new(honeycomb::Honeycomb::from_config(&config)?)
+        } else {
+            Box::new(DummyLogger)
+        };
+
+        let capsule = Capsule::new(&config, backend.as_ref(), logger.as_ref());
+
+        capsule.run_capsule(program_run_ref).await
+    }
+    .await;
 
     match result {
         Ok(exit_code) => {
