17647273e1a15cf9ca3f0b1974a71e504131bbd0
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-12-22 15:19:25 +0100

    Working binary that runs something like 'cargo test' for a single target.

--- cargo-capsule/src/bin/cargo-capsule-test.rs
+++ cargo-capsule/src/bin/cargo-capsule-test.rs
@@ -1,3 +1,57 @@
+use anyhow::{self, Error};
+use cargo::ops;
+use cargo::util::command_prelude::*;
+use cargo::util::config;
+use cargo::util::errors::CliError;
+use cargo::core::shell::Shell;
+
+use cargo_util;
+
+fn exec(config: &mut Config) -> CliResult {
+    let arg_matches = App::new("cargo-capsule-test")
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(Arg::with_name("wtf").help("WTF is that arg"));
+
+    let args = arg_matches.get_matches();
+    let ws = args.workspace(&config)?;
+
+    let ops = ops::TestOptions {
+        no_run: false,
+        no_fail_fast: false,
+        compile_opts: ops::CompileOptions::new(&config, CompileMode::Test)?,
+    };
+
+    let test_args: Vec<&'static str> = vec![];
+
+    let err = ops::run_tests(&ws, &ops, &test_args)?;
+    match err {
+        None => Ok(()),
+        Some(err) => {
+            let context = anyhow::format_err!("{}", err.hint(&ws, &ops.compile_opts));
+            let e = match err.code {
+                // Don't show "process didn't exit successfully" for simple errors.
+                Some(i) if cargo_util::is_simple_exit_code(i) => CliError::new(context, i),
+                Some(i) => CliError::new(Error::from(err).context(context), i),
+                None => CliError::new(Error::from(err).context(context), 101),
+            };
+            Err(e.into())
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = match config::Config::default() {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            let mut shell = Shell::new();
+            cargo::exit_with_error(e.into(), &mut shell)
+        }
+    };
+
+
+    let result = exec(&mut config);
+    match result {
+        Err(e) => cargo::exit_with_error(e, &mut *config.shell()),
+        Ok(()) => {}
+    }
 }
