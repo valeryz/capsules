71641353b08f7dfea3bf1396bfcb35564de9e6d2
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-12-27 17:29:10 +0100

    finding out dependencies and turning into flags

--- cargo-capsule/src/bin/cargo-capsule-test.rs
+++ cargo-capsule/src/bin/cargo-capsule-test.rs
@@ -1,9 +1,13 @@
-use anyhow::{self, Error};
+use std::collections::HashSet;
+
+use anyhow::{anyhow, Result, Error};
+use cargo::core::Source;
 use cargo::ops;
 use cargo::util::command_prelude::*;
 use cargo::util::config;
 use cargo::util::errors::CliError;
 use cargo::core::shell::Shell;
+use cargo::core::compiler::{UnitInterner, unit_graph, BuildContext};
 
 use cargo_util;
 
@@ -16,8 +20,6 @@ fn exec(config: &mut Config) -> CliResult {
     let args = arg_matches.get_matches_safe()?;
     let ws = args.workspace(&config)?;
 
-    println!("Workspace: \n{:?}", ws);
-
     let mut compile_opts = args.compile_options(
         config,
         CompileMode::Test,
@@ -25,13 +27,50 @@ fn exec(config: &mut Config) -> CliResult {
         ProfileChecking::Custom,
     )?;
 
+    let test_args: Vec<&'static str> = vec![];
+
+    println!("Workspace: \n{:?}\n", ws);
+
+    let interner = UnitInterner::new();
+    let BuildContext { ref roots, ref unit_graph, .. } = ops::create_bcx(&ws, &compile_opts, &interner)?;
+
+    // let _ = unit_graph::emit_serialized_unit_graph(&bcx.roots, &bcx.unit_graph, ws.config())?;
+    let empty_deps = Vec::new();
+    for root in roots {
+        let mut deps: Vec<_> = unit_graph.get(root).unwrap_or(&empty_deps).iter().map(|unit_dep| &unit_dep.unit).collect();
+        deps.push(&root);
+
+        let inputs : HashSet<(&str, String)> = deps.iter().map(|dep| -> Result<Vec<(&str, String)>> {
+            if dep.is_local() {
+                // Find all files
+                let pkg = &dep.pkg;
+                let mut src = cargo::sources::PathSource::new(pkg.root(), pkg.package_id().source_id(), config);
+                src.update()?;
+                src.list_files(pkg)?.iter().map(
+                    |file| 
+                        match file.as_os_str().to_str() {
+                            Some(s) => Ok(("-i", s.to_string())),
+                            None => Err(anyhow!("invalid path"))
+                        }).collect::<Result<_>>()
+            } else {
+                Ok(vec![("-t", dep.pkg.package_id().to_string())])
+            }
+        }).flatten().flatten().collect();
+
+        println!("Inputs for {:?} : {:?}\n\n", root, inputs.iter().map(|(a, b)| format!("{} {} ", a, b)).collect::<Vec<_>>());
+        
+    }
+
+    // Look at each 'root'. For each root, find all its transitive deps.
+    // For the transitive deps that are outside the workspace, represent them as tool tags.
+    // for the deps that are inside the workspace, find all their sources, and include as -i.
+    // Call cargo test -p 'target' under capsule with all these inputs.
+    
     let ops = ops::TestOptions {
         no_run: false,
         no_fail_fast: false,
         compile_opts: compile_opts,
     };
-
-    let test_args: Vec<&'static str> = vec![];
 
     let err = ops::run_tests(&ws, &ops, &test_args)?;
     match err {
